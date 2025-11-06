4132711031052870fa6f1b6ca0b91d6782fa8001
tejeshvarma071 <kallepalli.tejeshvarma07@gmail.com>
2025-11-06 09:50:33 +0530

    Update 0-1 KNAPSACK.c

--- LAB 12/0-1 KNAPSACK.c
+++ LAB 12/0-1 KNAPSACK.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
 
 int max(int a, int b) {
-return (a &gt; b) ? a : b;
+    return (a > b) ? a : b;
 }
 
 int main() {
-int n, W;
-printf(&quot;Enter number of items: &quot;);
-scanf(&quot;%d&quot;, &amp;n);
+    int n, W;
+    printf("Enter number of items: ");
+    scanf("%d", &n);
 
-int wt[n], val[n];
-printf(&quot;Enter weights of items: &quot;);
-for(int i = 0; i &lt; n; i++)
-scanf(&quot;%d&quot;, &amp;wt[i]);
+    int wt[n], val[n];
+    printf("Enter weights of items: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &wt[i]);
 
-printf(&quot;Enter values of items: &quot;);
-for(int i = 0; i &lt; n; i++)
-scanf(&quot;%d&quot;, &amp;val[i]);
+    printf("Enter values of items: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &val[i]);
 
-printf(&quot;Enter knapsack capacity: &quot;);
-scanf(&quot;%d&quot;, &amp;W);
+    printf("Enter knapsack capacity: ");
+    scanf("%d", &W);
 
-int dp[n+1][W+1];
-for(int i = 0; i &lt;= n; i++) {
-for(int w = 0; w &lt;= W; w++) {
-if(i == 0 || w == 0)
-dp[i][w] = 0;
-else if(wt[i-1] &lt;= w)
-dp[i][w] = max(val[i-1] + dp[i-1][w - wt[i-1]], dp[i-1][w]);
-else
-dp[i][w] = dp[i-1][w];
+    int dp[n + 1][W + 1];
+
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+            else
+                dp[i][w] = dp[i - 1][w];
+        }
+    }
+
+    printf("Maximum value that can be obtained = %d\n", dp[n][W]);
+    return 0;
 }
-}
-printf(&quot;Maximum value that can be obtained = %d\n&quot;, dp[n][W]);
-return 0;
-}
+
